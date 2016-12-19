8fff3eceb06a181f486f0f7ba06daa9b38ac2140
Dan Pidcock <dpidcock@marexspectron.com>
2016-12-19 17:37:04 +0000

    Rename to dpbb for npm

--- examples/hello_world.js
+++ examples/hello_world.js
@@ -3,12 +3,19 @@
 // add timestamps in front of log messages
 require('console-stamp')(console, 'HH:MM:ss');
 
-var bb = require('../bb.js');
+var bb = require('dpbb');
 var fs = require('fs')
 
+// Get directory from command line argument
 var progArgs = process.argv.slice(2);
 var sourceDir = progArgs[0];
+if (!sourceDir) {
+  console.log('Directory must be specified.');
+  process.exit()
+}
 
+// Create a chain of processors, that together
+// will print Hello World
 var processors = [
   function() {
     console.log('Hello');
@@ -21,4 +28,5 @@ var processors = [
 ];
 bb.create(processors);
 
+// Start watching the directory with the processor chain
 fs.watch(sourceDir, function(eventType) { bb.handleChange(); });
