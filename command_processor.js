8f7a2006f60f3c3d9122c0b23ebc9fee04986489
Dan Pidcock <dpidcock@marexspectron.com>
2017-12-21 10:11:18 +0000

    chained wip

--- command_processor.js
+++ command_processor.js
@@ -22,3 +22,28 @@ function createCommandProcessor(command) {
 exports.createCommandProcessors = function(commands, workingDir) {
   return commands.map(createCommandProcessor, workingDir);
 }
+
+function createTypedCommandProcessor(command_pair) {
+  var workingDir = this;
+  var processor = function() {
+    var command_type = command_pair[0];
+    try {
+      var command = command_pair[1];
+      console.log(command);
+      execSync(command, {'cwd': workingDir});
+      return true;
+    }
+    catch(err) {
+      if (command_type == 'out') {
+        console.log(String(err.stdout))
+      }
+      console.log(err.cmd, 'Failed with error', err.status);
+      return false;
+    }
+  };
+  return processor;
+}
+
+exports.createTypedCommandProcessors = function(commands, workingDir) {
+  return commands.map(createTypedCommandProcessor, workingDir);
+}
